824129b1fc842e469893aa20dc93e26120a6d126
Mo <moh19951@outlook.com>
2019-11-21 14:46:39 +0100

    Uitwerking van de exercise

---
+++ rijbewijs.php
@@ -0,0 +1,9 @@
+<?php
+echo "Hoe oud ben je ";
+$a = readline(">");
+if ($a < "16.5") {
+    echo "Helaas je mag nog niet auto rijden je moet nog even wachten zoomer ";
+} else {
+    echo "Je mag beginnen met rijlessen boomer";
+}
+?>
